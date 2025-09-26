29468512807a4f57a92571916e78a09eadd3bda7
Déborah Malheiro <malheirodev@gmail.com>
2025-09-26 18:16:19 -0300

    test: recommendation list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Form from './components/Form/Form';
-import RecommendationList from './components/RecommendationList/RecommendationList';
+import RecommendationList from './components/RecommendationList';
 
 function App() {
   const [recommendations, setRecommendations] = useState([])
---
+++ frontend/src/components/RecommendationList/RecommendationList.test.js
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import RecommendationList from './RecommendationList';
+
+describe('RecommendationList component', () => {
+  it('deve renderizar o title do componente', () => {
+    render(<RecommendationList recommendations={[]} />);
+
+    const title = screen.getByText('Lista de Recomendações:');
+    expect(title).toBeInTheDocument();
+  });
+
+  it('deve exibir texto quando não existir recomendações', () => {
+    render(<RecommendationList recommendations={[]} />);
+
+    const message = screen.getByText('Nenhuma recomendação encontrada.');
+    expect(message).toBeInTheDocument();
+  });
+
+  it('deve renderizar a lista de recomendações', () => {
+    const mockRecommendations = [
+      { name: 'Produto A' },
+      { name: 'Produto B' },
+    ];
+
+    render(<RecommendationList recommendations={mockRecommendations} />);
+
+    const items = screen.getAllByRole('listitem');
+    expect(items).toHaveLength(2);
+    expect(items[0]).toHaveTextContent('Produto A');
+    expect(items[1]).toHaveTextContent('Produto B');
+  });
+
+  it('não deve exibir mensagem de "nenhuma recomendação" quando existirem recomendações', () => {
+    const mockRecommendations = [{ name: 'Produto A' }];
+
+    render(<RecommendationList recommendations={mockRecommendations} />);
+
+    const message = screen.queryByText('Nenhuma recomendação encontrada.');
+    expect(message).not.toBeInTheDocument();
+  });
+});
