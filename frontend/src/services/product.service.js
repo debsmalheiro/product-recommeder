f506bc4e8ba1063fb55a7cc8fdcb7099304f4182
Déborah Malheiro <malheirodev@gmail.com>
2025-09-26 15:01:28 -0300

    refactor: remove axios and use fetch api

--- frontend/src/services/product.service.js
+++ frontend/src/services/product.service.js
@@ -1,11 +1,15 @@
-import axios from 'axios';
-
 const baseURL = 'http://localhost:3001';
 
 const getProducts = async () => {
   try {
-    const response = await axios.get(`${baseURL}/products`);
-    return response.data;
+    const response = await fetch(`${baseURL}/products`);
+
+    if (!response.ok) {
+      throw new Error(`Erro ao obter os produtos: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Erro ao obter os produtos:', error);
     throw error;
